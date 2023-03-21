31ce2ab26d5e3da1ad658505f1127cc33ae6ec82
Your Name <you@example.com>
2023-03-21 12:14:41 +0100

    ,

--- lotto.py
+++ lotto.py
@@ -1,8 +1,14 @@
-print("wybiez numer od 1do 49")
+for q in range(6):
+    print("wybiez numer od 1do 49")
 import random
 liczby=[]
 for i in range(6):
     los=random.randint(1, 49)
     print(f"wylosowana liczba nr{i+1}")
+    if los in liczby:
+        los=random.randint(1, 49)
+    print (los)
     liczby.append(los)
+
+    
 
