f0208384d5a7c0cc37a0571e8e3ddb2ddcf91ac8
Your Name <you@example.com>
2023-04-13 12:15:53 +0200

    .

--- kalkulator.py
+++ kalkulator.py
@@ -1,5 +1,6 @@
 
-def calc(operator, x, y):
+def calc(operator, x, y):       
+
     if operator == "+":
         return x + y
     elif operator == "-":
@@ -7,6 +8,18 @@ def calc(operator, x, y):
     elif operator == "*":
         return x * y
     elif operator == "/":
-        return x / y
+       if y == 0:
+            return print("niemoźna dzielić przez 0")
+       else:
+            return x / y
+       
     else:
         return None
+print("podaj x")    
+x=int(input())
+print("podaj operator") 
+operator=input()
+print("podaj y") 
+y=int(input())
+
+print(calc(operator, x, y))
