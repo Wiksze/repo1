61f3bd1b8ccb425652affab98b14e5458d53abb4
Your Name <you@example.com>
2023-05-11 10:14:52 +0200

    ,

--- liczby rzymskie.py
+++ liczby rzymskie.py
@@ -2,15 +2,19 @@
 jednosci = ["", "I", "II", "III", "IV", "V", "VI", "VII", "IIX", "IX"]
 dziesiatki = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "XXC", "CX"]
 setki = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "CCM", "CM"]
-tys = ["M","MM","MMM","Mↁ","ↁ","ↁM","ↁMM","ↁMMM"]
-dtys =int["ↂ"]
+tys = ["","M","MM","MMM","Mↁ","ↁ","ↁM","ↁMM","ↁMMM"]
+dtys = ["","ↂ ","ↂ ↂ ","ↂ ↂ ↂ "]
 
+print("tylko liczby mniejsze od 30000!")
 cyfry = input("podaj liczbę\n")
-while len(cyfry) < 4:
+while len(cyfry) < 5:
 	cyfry = "0" + cyfry
-print (cyfry[int(cyfry[-5])] * "ↂ" + tys[int(cyfry[-4])] + setki[int(cyfry[-3])] + dziesiatki[int(cyfry[-2])] + jednosci[int(cyfry[-1])])
+	print(cyfry)
+
+
+print (dtys[int(cyfry[-5])] + tys[int(cyfry[-4])] + setki[int(cyfry[-3])] + dziesiatki[int(cyfry[-2])] + jednosci[int(cyfry[-1])])
 
 
 
 
-
+   
