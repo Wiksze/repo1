189acc399175410adbb55572a0f1dddf2bc78e1e
Your Name <you@example.com>
2023-05-16 12:09:19 +0200

    w

--- pola_figur.py
+++ pola_figur.py
@@ -1,29 +1,4 @@
 import math
-def calc(operator, x, y):       
-
-    if operator == "+":
-        return x + y
-    elif operator == "-":
-        return x - y
-    elif operator == "**":
-        return x ** y
-    elif operator == "%":
-        if y==0:
-             return print("niemoźna dzielić przez 0")
-        else:
-            return x % y
-    
-    elif operator == "*":
-        return x * y
-    elif operator == "/":
-       if y == 0:
-            return print("niemoźna dzielić przez 0")
-       else:
-            return x / y
-       
-    else:
-        return None
-
 def calc(figura):       
 
     if figura == "kwadrat":
@@ -72,7 +47,7 @@ def calc(figura):
         print("podaj h")
         h=int(input())
         return a * h
-    elif figura == "elipsaeli":
+    elif figura == "elipsa":
         print("podaj a")
         a=int(input())
         print("podaj b")
